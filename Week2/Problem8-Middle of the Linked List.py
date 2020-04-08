6b8834127c997558e63283ca2903447e2af67cef
komalupatil31 <ku.patil31@gmail.com>
2020-04-08 00:51:38 -0700

    adding solution for problem 8

---
+++ Week2/Problem8-Middle of the Linked List.py
@@ -0,0 +1,67 @@
+#Problem8
+#Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+#If there are two middle nodes, return the second middle node.
+#Example 1:
+#Input: [1,2,3,4,5]
+#Output: Node 3 from this list (Serialization: [3,4,5])
+#The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+#Note that we returned a ListNode object ans, such that:
+#ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+#Example 2:
+#Input: [1,2,3,4,5,6]
+#Output: Node 4 from this list (Serialization: [4,5,6])
+#Since the list has two middle nodes with values 3 and 4, we return the second one.
+#Note:
+#The number of nodes in the given list will be between 1 and 100.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+#Solution1
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        if head == None:
+            return None
+        
+        temp = head
+        temp2 = head
+        count = 0
+        
+        while (temp!=None):
+            count +=1 
+            temp = temp.next
+            
+        if count%2 == 0:
+            mid = (count//2)
+        else:
+            mid = count//2
+        
+        for i in range(0, mid):
+            temp2 = temp2.next
+        
+        return temp2
+
+
+#Solution2
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        if head == None:
+            return None
+        slow = head
+        fast = head
+        while(fast != None and fast.next != None):
+            fast = fast.next.next
+            slow = slow.next
+        return slow
