56f5910f98e20575d290ab71ccb69b74378270bf
komalupatil31 <ku.patil31@gmail.com>
2020-04-25 22:57:18 -0700

    added solution for problem 25

---
+++ Week4/Problem25-Jump Game.py
@@ -0,0 +1,26 @@
+#Problem25
+#Given an array of non-negative integers, you are initially positioned at the first index of the array.
+#Each element in the array represents your maximum jump length at that position.
+#Determine if you are able to reach the last index.
+#Example 1:
+#Input: [2,3,1,1,4]
+#Output: true
+#Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+#Example 2:
+#Input: [3,2,1,0,4]
+#Output: false
+#Explanation: You will always arrive at index 3 no matter what. Its maximum
+#jump length is 0, which makes it impossible to reach the last index.
+
+#Solution - jump taken must be atleast equal to the index
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        reachjump = 0
+        
+        for i in range(len(nums)):
+            if i > reachjump:
+                return False
+            reachjump = max(reachjump, i + nums[i])
+        return True
